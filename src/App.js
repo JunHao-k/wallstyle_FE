4ceb71110371fd7530bf4ddb6b05053bafe9dc44
JunHao-k <koh.junhao@trentglobal.edu.sg>
2022-08-28 09:43:50 +0000

    Working on products

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Cart from "./pages/Cart"
@@ -11,6 +12,17 @@ import Banner from "./components/Banner"
 
 
 function App() {
+  const BASE_URL = "https://wall-style.herokuapp.com/api"
+  const [products , setProducts] = useState([])
+  
+  useEffect(() => {
+    const getAllProducts = async () => {
+      let response = await axios.get("BASE_URL" + "/products")
+      setProducts(response.data)
+    }
+    getAllProducts()
+  } , [])
+
   return (
     <React.Fragment>
       <Router>
